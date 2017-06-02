6459d7a2ab857a1ba148c2ac9bbb6a610e436a73
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2017-06-02 15:38:30 +0200

    Use new api

--- alexandria/notebook/alexandria_markup_proxy.py
+++ alexandria/notebook/alexandria_markup_proxy.py
@@ -9,13 +9,23 @@ class AlexandriaMarkupProxy:
         self.latexutil = LaTeXUtil(latex_server_url)
 
     def __dir__(self):
-        return ['add_document_from_lmnl_file', 'add_document_from_lmnl_text']
+        return ['add_document_from_lmnl_file', 'add_document_from_texmecs_file',
+                'add_document_from_lmnl_text', 'add_document_from_texmecs_text']
 
     def add_document_from_lmnl_file(self, path):
         with open(path, 'r') as f:
             lmnl = f.read()
         return self.add_document_from_lmnl_text(lmnl)
 
+    def add_document_from_texmecs_file(self, path):
+        with open(path, 'r') as f:
+            texmecs = f.read()
+        return self.add_document_from_texmecs_text(texmecs)
+
     def add_document_from_lmnl_text(self, lmnl):
-        uuid = self.alexandria.documents.add(lmnl).uuid
+        uuid = self.alexandria.documents.add_from_lmnl(lmnl).uuid
+        return DocumentProxy(uuid, self.alexandria, self.latexutil)
+
+    def add_document_from_texmecs_text(self, texmecs):
+        uuid = self.alexandria.documents.add_from_texmecs(texmecs).uuid
         return DocumentProxy(uuid, self.alexandria, self.latexutil)
