67e9b2f9d81b5cc405f175606f3f0d6ba0b319fd
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2017-07-07 15:49:47 +0200

    [NLA-405] Send latex utf-8 encoded to tex-util-server

--- alexandria/notebook/latex_util.py
+++ alexandria/notebook/latex_util.py
@@ -10,6 +10,10 @@ class LaTeXUtil:
     def svg_uri(self, tex):
         url = urljoin(self.base_uri, "2svg")
         session = requests.Session()
-        session.headers['content-type'] = 'text/plain'
-        response = session.post(url, data=tex)
-        return response.headers['location']
+        session.headers['content-type'] = 'text/plain; charset=UTF-8'
+        response = session.post(url, data=tex.encode('utf-8'))
+        if 'location' in response.headers:
+            svg_uri = response.headers['location']
+            return svg_uri
+        else:
+            raise ValueError('tex-util-server returned an error: ', response.json())
